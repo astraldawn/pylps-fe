8466c7996829fbfc5bc965b14e6c3839a3d494f3
Mark <mark.lee.chu.yong@gmail.com>
2018-02-08 14:32:07 +0000

    add action defer example

--- src/app/programs.ts
+++ src/app/programs.ts
@@ -112,5 +112,34 @@ false_if(
 
 execute()
 
+show_kb_log()`,
+
+ACTION_DEFER:
+`# ACTION DEFER
+from pylps.core import *
+
+initialise(max_time=2)
+
+create_facts('f(_)', 'g(_)')
+create_actions('p1(_)', 'p2(_)')
+create_variables('X', 'Y')
+
+f(1)
+f(2)
+g(1)
+
+reactive_rule(f(X)).then(
+    p1(X).frm(T1, T2)
+)
+
+reactive_rule(g(X)).then(
+    p2(X).frm(T1, T2)
+)
+
+false_if(p1(X), p2(Y), X != Y)
+
+execute()
+
 show_kb_log()`
+
 });
