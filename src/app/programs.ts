22bb6bf7594d070319f0808d308145383668d28c
Mark <mark.lee.chu.yong@gmail.com>
2018-04-12 00:21:39 +0100

    added blocks world

--- src/app/programs.ts
+++ src/app/programs.ts
@@ -229,5 +229,82 @@ execute(single_clause=False)
 
 show_kb_log()`,
 
+BLOCKS_WORLD:
+`from pylps.core import *
+from pylps.lps_data_structures import LPSConstant
+
+initialise(max_time=10)
+
+create_fluents('location(_, _)')
+create_actions('move(_, _)', 'say(_)')
+create_events(
+    'clear(_)', 'make_tower(_)',
+    'make_on(_, _)', 'make_clear(_)',
+)
+create_variables('Block', 'Block1', 'Place', 'Places')
+
+initially(
+    location('f', 'floor'), location('b', 'f'), location('e', 'b'),
+    location('a', 'floor'), location('d', 'a'), location('c', 'd'),
+)
+
+reactive_rule(True).then(
+    make_tower(['a', 'b', 'c', 'floor']).frm(T1, T2),
+)
+
+reactive_rule(True).then(
+    make_tower(['f', 'e', 'd', 'floor']).frm(T1, T2),
+)
+
+
+goal(clear(Block).at(T)).requires(
+    Block != 'floor',
+    ~location(_, Block).at(T),
+)
+
+goal(clear('floor').at(_))
+
+goal(
+    make_tower([Block | LPSConstant('floor')]).frm(T1, T2)
+).requires(
+    make_on(Block, 'floor').frm(T1, T2),
+)
+
+goal(
+    make_tower([Block | [Place | Places]]).frm(T1, T3)
+).requires(
+    Place != 'floor',
+    make_tower([Place | Places]).frm(T1, T2),
+    make_on(Block, Place).frm(T2, T3),
+)
+
+goal(make_on(Block, Place).frm(T1, T2)).requires(
+    ~location(Block, Place).at(T1),
+    make_clear(Place).frm(T1, T2),
+    make_clear(Block).frm(T2, T3),
+    move(Block, Place).frm(T3, T4),
+)
+
+goal(make_on(Block, Place).frm(T, T)).requires(
+    location(Block, Place).at(T),
+)
+
+goal(make_clear(Place).frm(T, T)).requires(
+    clear(Place).at(T),
+)
+
+goal(make_clear(Block).frm(T1, T2)).requires(
+    location(Block1, Block).at(T1),
+    make_on(Block1, 'floor').frm(T1, T2),
+)
+
+move(Block, Place).initiates(location(Block, Place))
+move(Block, _).terminates(location(Block, Place))
+
+execute()
+
+show_kb_log()
+`,
+
 
 });
